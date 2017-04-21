d154063464746166b145df67ce1353e78c25f84f
Marcos Lluberes <marcos.antonio.lluberes@hotmail.com>
2017-04-21 13:22:51 -0400

    Commit  de la aplicación
    Publicación de la aplicación en el repositorio público alojado en la
    plataforma de GitHub.

---
+++ ProyectoFinal/SignalRChat/Common/MessageDetail.cs
@@ -0,0 +1,11 @@
+﻿namespace ProyectoFinal.Common
+{
+    public class MessageDetail
+    {
+
+        public string UserName { get; set; }
+
+        public string Message { get; set; }
+    
+    }
+}
